d823be8bfc176e0a2e274ae848a9dee0d2cee44c
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-15 11:48:00 -0600

    Create Sprite Kit Project

---
+++ Airplane/Airplane/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Airplane
+//
+//  Created by Cassandra Sandquist on 3/15/2014.
+//  Copyright (c) 2014 Cassandra Sandquist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Airplane/AirplaneTests/AirplaneTests.m
@@ -0,0 +1,34 @@
+//
+//  AirplaneTests.m
+//  AirplaneTests
+//
+//  Created by Cassandra Sandquist on 3/15/2014.
+//  Copyright (c) 2014 Cassandra Sandquist. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AirplaneTests : XCTestCase
+
+@end
+
+@implementation AirplaneTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
