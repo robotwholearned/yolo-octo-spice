98145772f58fa72a173a732eb53d7fe2d91452e3
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-19 13:57:32 -0600

    show menu scene

--- Missile/MissileComand/MissileComand/MenuScene.m
+++ Missile/MissileComand/MissileComand/MenuScene.m
@@ -14,10 +14,14 @@
 {
     if (self = [super initWithSize:size])
     {
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        //self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        self.backgroundColor = [SKColor colorWithRed:(198.0/255.0) green:(220.0/255.0) blue:(54.0/255.0) alpha:1.0];
         SKSpriteNode *title = [SKSpriteNode spriteNodeWithImageNamed:@"title"];
         title.zPosition = 2;
+        title.scale = 0.5;
         title.position = CGPointMake(size.width/2, size.height/2);
+        
+        title.blendMode = SKBlendModeAdd;
         
         [self addChild:title];
     }
